feb05ab45b78196560b320ae931f2657fec81860
adarshahelvar <adarshaadi1997@gmail.com>
2023-11-23 17:18:26 +0530

    V1 Update

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,4 @@
-export const BASE_URL = "http://localhost:5000/api/v1"
+// export const BASE_URL = "http://localhost:5000/api/v1"
+export const BASE_URL = "https://adarshahelvar-blog-api.cyclic.app/api/v1";
 
-export const token = localStorage.getItem("token")
+export const token = localStorage.getItem("token");
